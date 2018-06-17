9d26bcbe4f745c973fe719ebfff052a419e25f99
Taras Hrynchuk <Taras_Hrynchuk@epam.com>
2018-06-17 14:41:50 +0300

    collection listener

--- src/main/java/guru/springframework/mongodbreactivestockquoteservice/QuoteRunner.java
+++ src/main/java/guru/springframework/mongodbreactivestockquoteservice/QuoteRunner.java
@@ -2,23 +2,33 @@ package guru.springframework.mongodbreactivestockquoteservice;
 
 import guru.springframework.mongodbreactivestockquoteservice.client.StockQuoteClient;
 import guru.springframework.mongodbreactivestockquoteservice.domain.Quote;
+import guru.springframework.mongodbreactivestockquoteservice.repositories.QuoteRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+import reactor.core.Disposable;
 import reactor.core.publisher.Flux;
 
-//@Component
+@Component
 public class QuoteRunner implements CommandLineRunner {
 
 	private final StockQuoteClient stockQuoteClient;
 
-	public QuoteRunner(StockQuoteClient stockQuoteClient) {
+	private final QuoteRepository quoteRepository;
+
+	public QuoteRunner(StockQuoteClient stockQuoteClient, QuoteRepository quoteRepository) {
 		this.stockQuoteClient = stockQuoteClient;
+		this.quoteRepository = quoteRepository;
 	}
 
 	@Override
 	public void run(String... strings) throws Exception {
-		Flux<Quote> quoteStream = stockQuoteClient.getQuoteStream();
+//		Flux<Quote> quoteStream = stockQuoteClient.getQuoteStream();
+		//		quoteStream.subscribe(System.out::println);
 
-		quoteStream.subscribe(System.out::println);
+		Flux<Quote> quoteFlux = quoteRepository.findWithTailableCursorBy();
+		Disposable disposable = quoteFlux.subscribe(quote -> {
+			System.out.println("================== quote Id: " + quote.getId());
+		});
+		//disposable.dispose();
 	}
 }
